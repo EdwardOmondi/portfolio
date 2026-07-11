aac2b502a42f2f6e5299821f1bdd18bf738a3c42
Edward Omondi <ewotieno470@gmail.com>
2026-07-11 21:09:59 +0300

    Updates

---
+++ _nuxt/Cjtxw5d_.js
@@ -0,0 +1,1 @@
+import{r as s}from"./DynEey_y.js";import e from"./yTzArKdw.js";import{a0 as t,b as a,w as p,e as c,o as n}from"./BkuYOLa4.js";import"./iik6CYzq.js";const d={__name:"ProseTip",setup(i){const o=t();return(r,f)=>(n(),a(e,{color:"success",icon:c(o).ui.icons.tip},{default:p(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
