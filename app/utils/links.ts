fe4c28f283438168d74c9ada2856679488a93286
Edward Omondi <ewotieno470@gmail.com>
2026-07-11 17:41:19 +0300

    Added CV to portfolio website

--- app/utils/links.ts
+++ app/utils/links.ts
@@ -1,23 +1,34 @@
 import type { NavigationMenuItem } from '@nuxt/ui'
 
-export const navLinks: NavigationMenuItem[] = [{
-  label: 'Home',
-  icon: 'i-lucide-home',
-  to: '/'
-}, {
-  label: 'Projects',
-  icon: 'i-lucide-folder',
-  to: '/projects'
-}, {
-  label: 'Blog',
-  icon: 'i-lucide-file-text',
-  to: '/blog'
-}, {
-  label: 'Certifications',
-  icon: 'i-lucide-award',
-  to: '/certification'
-}, {
-  label: 'About',
-  icon: 'i-lucide-user',
-  to: '/about'
-}]
+export const navLinks: NavigationMenuItem[] = [
+  {
+    label: 'Home',
+    icon: 'i-lucide-home',
+    to: '/'
+  },
+  {
+    label: 'Projects',
+    icon: 'i-lucide-folder',
+    to: '/projects'
+  },
+  {
+    label: 'Blog',
+    icon: 'i-lucide-file-text',
+    to: '/blog'
+  },
+  {
+    label: 'Certifications',
+    icon: 'i-lucide-award',
+    to: '/certification'
+  },
+  {
+    label: 'About',
+    icon: 'i-lucide-user',
+    to: '/about'
+  },
+  {
+    label: 'CV',
+    icon: 'i-lucide-user',
+    to: '/cv'
+  }
+]
